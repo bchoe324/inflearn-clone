94dcc1d7f994516b771c1781a76c98eac4ce645a
boramchoi <bchoe324@gmail.com>
2025-08-26 17:34:13 +0900

    next-auth 로그인 로직 추가

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -1,8 +1,61 @@
 import NextAuth from "next-auth";
 import { prisma } from "./prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { comparePassword } from "./lib/password-utils";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  useSecureCookies: process.env.NODE_ENV === "production",
+  trustHost: true,
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "이메일",
+          type: "email",
+          placeholder: "이메일 입력",
+        },
+        password: {
+          label: "비밀번호",
+          type: "password",
+          placeholder: "비밀번호 입력",
+        },
+      },
+      async authorize(credentials) {
+        // 1. 모든 값이 정상적으로 들어왔는지
+        if (!credentials || !credentials.email || !credentials.password) {
+          throw new Error("이메일과 비밀번호를 입력해주세요.");
+        }
+
+        // 2. DB에서 유저 찾기
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) {
+          throw new Error("존재하지 않는 이메일입니다.");
+        }
+
+        // 3. 비밀번호 일치 여부 확인
+        const passwordMatch = comparePassword(
+          credentials.password as string,
+          user.hashedPassword as string
+        );
+
+        if (!passwordMatch) {
+          throw new Error("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
 });
---
+++ frontend/lib/password-utils.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcryptjs";
+
+// salt hash pw
+export function saltAndHashPassword(password: string): string {
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  return hash;
+}
+
+export function comparePassword(
+  password: string,
+  hashedPassword: string
+): boolean {
+  return bcrypt.compareSync(password, hashedPassword);
+}
